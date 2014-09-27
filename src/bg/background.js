d9cf428c4d3f27234ebe84588a7099cc17a89947
Ian Webster <typppo+github@gmail.com>
2014-09-27 13:01:01 -0400

    Icons and final description, etc!

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,7 +4,7 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (tab.active) {
     var sound = document.getElementById('bgsound');
     if (changeInfo.status == 'complete') {
-      if (Math.random() * 100 < 10) {
+      if (Math.random() * 100 < .5) {
         sound.pause();
         sound.currentTime = 0;
         sound.play();
