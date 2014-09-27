8ce4c58a518edf73ad43244c4b63c7d2f6049832
Ian Webster <typppo+github@gmail.com>
2014-09-27 12:50:43 -0400

    Working random lion king!

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,25 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+var currentTabId = -1;
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (tab.active) {
+    var sound = document.getElementById('bgsound');
+    if (changeInfo.status == 'complete') {
+      if (Math.random() * 100 < 10) {
+        sound.pause();
+        sound.currentTime = 0;
+        sound.play();
+        currentTabId = tabId;
+      }
+    } else {
+      sound.pause();
+      currentTabId = -1;
+    }
+  }
+})
 
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+chrome.tabs.onRemoved.addListener( function (tabId, changeInfo, tab) {
+  if (tabId === currentTabId) {
+    var sound = document.getElementById('bgsound');
+    sound.pause();
+  }
+})
