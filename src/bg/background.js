635049e6b21a91bc3ed25f4e063089b82aa5f464
Ian Webster <iwebster@google.com>
2014-10-13 14:05:41 -0700

    reduce frequency to 1 in 400

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,7 +4,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (tab.active) {
     var sound = document.getElementById('bgsound');
     if (changeInfo.status == 'complete') {
-      if (Math.random() * 100 < .5) {
+      if (Math.random() * 100 < .25) {
         sound.pause();
         sound.currentTime = 0;
         sound.play();
